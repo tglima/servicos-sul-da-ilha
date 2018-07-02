2af5012fa6167bc3e1609949b2854053454c4d0c
tglima <thg.limadesousa@gmail.com>
2018-07-02 08:44:14 -0300

    Adiciona o componente bottom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopComponent } from './components/top/top.component';
+import { BottomComponent } from './components/bottom/bottom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopComponent
+    TopComponent,
+    BottomComponent
   ],
   imports: [
     NgbModule.forRoot(),
