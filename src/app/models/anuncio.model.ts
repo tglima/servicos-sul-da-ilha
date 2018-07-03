6a2402187aa1f9b41690debc74d8d2f6dbb1ac16
tglima <thg.limadesousa@gmail.com>
2018-07-03 08:33:18 -0300

    Adiciona o model Anuncio

---
+++ src/app/models/anuncio.model.ts
@@ -0,0 +1,12 @@
+export class Anuncio {
+    public id: number;
+    public anunciante: string;
+    public categoria: string;
+    public titulo: string;
+    public descricao: string;
+    public telefone: string;
+    public whatsapp: boolean;
+    public destaque: boolean;
+    public status: string;
+    public imagens: Array<Object>;
+}
