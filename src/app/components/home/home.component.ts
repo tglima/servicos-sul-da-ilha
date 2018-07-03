92c1d325f7bf97351532c0f975082d5457003300
tglima <thg.limadesousa@gmail.com>
2018-07-03 20:32:12 -0300

    Atualiza o componente home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AnunciosService } from '../../services/anuncios.service';
+import { Anuncio } from '../../models/anuncio.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ AnunciosService ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public anuncios: Array<Anuncio>;
+
+  constructor(private anunciosService: AnunciosService) { }
 
   ngOnInit() {
+    this.anunciosService.getAnuncios()
+    .then((anuncios: Anuncio[] ) => {
+      this.anuncios = anuncios;
+    }).catch((param: any) => {
+      console.log('Error ao carregar os anúncios: \n', param);
+    });
   }
 
 }
