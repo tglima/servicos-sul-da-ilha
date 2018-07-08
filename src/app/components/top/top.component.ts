e6cb4a6e5ea7b8fd385e1f0aba8d939cf96faa9f
tglima <thg.limadesousa@gmail.com>
2018-07-08 10:52:16 -0300

    Atualiza o componente top

--- src/app/components/top/top.component.ts
+++ src/app/components/top/top.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnunciosService } from '../../services/anuncios.service';
+import { Observable, Subject, of } from 'rxjs';
+import { Anuncio } from '../../models/anuncio.model';
+import { debounceTime, distinctUntilChanged, switchMap, catchError, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
-  styleUrls: ['./top.component.css']
+  styleUrls: ['./top.component.css'],
+  providers: [ AnunciosService ]
 })
-export class TopComponent {
+export class TopComponent implements OnInit {
 
-  constructor() { }
-
+  showResult: boolean;
   isCollapsed = true;
+  anuncios: Observable<Anuncio[]>;
+  private subjectPesquisa = new Subject<String>();
+
+  constructor(private anunciosService: AnunciosService) { }
+
+  ngOnInit() {
+    this.anuncios = this.subjectPesquisa
+    .pipe(
+            tap(() => { this.showResult = false; }),
+            debounceTime(750),
+            distinctUntilChanged(),
+            switchMap((termo: string) => {
+              if (termo.trim() === '') {
+                return of(  <Anuncio[]>([]) );
+              }
+              this.showResult = true;
+              return this.anunciosService.pesquisarAnuncio(termo);
+            }),
+            catchError(err => {
+              throw new Error(err.message);
+            })
+    );
+  }
+
+  public pesquisar(termoDaBusca: string): void {
+    this.subjectPesquisa.next(termoDaBusca);
+  }
+
+  public limparPesquisa(): void {
+    this.subjectPesquisa.next('');
+    this.showResult = false;
+  }
 
 }
