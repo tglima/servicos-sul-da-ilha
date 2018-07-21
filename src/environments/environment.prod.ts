2c3c265f357cf603250a8c2035ec5dcfe542e933
tglima <thg.limadesousa@gmail.com>
2018-07-21 11:41:15 -0300

    Atualiza  environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
 
-    // URL of development api
-    apiUrl: 'http://192.168.0.15:3000/'
+    /* Substitua o valor abaixo pelo IP da sua máquina na rede.
+       Por exemplo:   apiUrl: 'http://192.168.0.10:3000'     */
+       
+    apiUrl: 'http://127.0.0.1:3000/'
 };
